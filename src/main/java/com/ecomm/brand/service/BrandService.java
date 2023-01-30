f496f79fd6c152b0fae3ee2d282b131357e990ea
Sahana20212 <96504836+Sahana20212@users.noreply.github.com>
2023-01-30 07:31:07 -0600

    Validation fixes

--- src/main/java/com/ecomm/brand/service/BrandService.java
+++ src/main/java/com/ecomm/brand/service/BrandService.java
@@ -1,5 +1,7 @@
 package com.ecomm.brand.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,11 @@ public class BrandService {
 	@Autowired
 	private BrandRepository brandrepository;
 
-	
-	public Brand saveBrand( Brand brand ) {
+	public Brand saveBrand(Brand brand) {
 		return brandrepository.save(brand);
-
-}
+	}
+	
+	public List<Brand> fetchBrandList() {
+		return brandrepository.findAll();
+	}
 }

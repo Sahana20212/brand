83e06a135e8035fd20d308b0d6e7ac194618eee7
Sahana20212 <96504836+Sahana20212@users.noreply.github.com>
2023-01-30 18:50:24 -0600

    Loggers

--- src/main/java/com/ecomm/brand/controller/BrandController.java
+++ src/main/java/com/ecomm/brand/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.ecomm.brand.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +17,22 @@ import jakarta.validation.Valid;
 @RestController
 public class BrandController {
 	
+	
+	org.slf4j.Logger logger = LoggerFactory.getLogger(BrandController.class);
 	@Autowired
 	private BrandService brandservice;
 	
 	@PostMapping(path="/brand")
 	public Brand saveBrand(@Valid @RequestBody Brand brand ) {
+		logger.info("enterting into brand controller Save Brand");
+		brand.getBrand_name().toCharArray();
 		return brandservice.saveBrand(brand);
 	}
 	
 	@GetMapping(path="/brand")
 	 public List<Brand> fetchBrandList() {
+		logger.info("enterting into brand controller fetchBrandList");
+		
 		List<Brand> a=brandservice.fetchBrandList();
 		return a;	
 	}
--- src/main/java/com/ecomm/brand/service/BrandService.java
+++ src/main/java/com/ecomm/brand/service/BrandService.java
@@ -2,6 +2,8 @@ package com.ecomm.brand.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,10 @@ import com.ecomm.brand.repository.BrandRepository;
 public class BrandService {
 	@Autowired
 	private BrandRepository brandrepository;
+    Logger log=LoggerFactory.getLogger(BrandService.class);
 
 	public Brand saveBrand(Brand brand) {
+		
 		return brandrepository.save(brand);
 	}
 	
